9603856cb09332d84d5a3236dc4cefa98de48d65
Oladele Marwan <oladelemarwan@gmail.com>
2022-10-28 18:10:45 +0100

    new commit

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const BUY_CAKE = "BUY_CAKE";
 const buyCake = () => {
   return {
     type: BUY_CAKE,
-    info: 'First Redux Action' 
+    info: "First Redux Action",
+  };
+};
+
+const initialState = {
+  numOfCake: 10,
+};
+
+const reducerFn = (state = initialState, action) => {
+  if (action.type === BUY_CAKE) {
+    return {
+      ...state,
+      numOfCake: state.numOfCake - 1,
+    };
   }
-}
+  return state;
+};
