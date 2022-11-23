29d58fad7746850d0e8af9aaf5f03f6cb979369d
MarwanOladele <oladelemarwan@gmail.com>
2022-11-23 20:21:36 +0100

    new commit

---
+++ src/component/CakeContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CakeContainer = () => {
+  return (
+    <div>
+        <h2>number of cakes</h2>
+        <button>Buy Cakes</button>
+    </div>
+  )
+}
+
+export default CakeContainer 
